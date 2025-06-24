86390241a4b002625c366f6f33705fd8f55bdc1e
Pooja Mehmi <poojamehmi012@gmail.com>
2025-06-24 14:15:00 +0530

    Delete src directory

--- src/components/TodoItems.js
+++
@@ -1,9 +0,0 @@
-export default function TodoItems({todo,onDelete}){
-    return(
-        <div className="container">
-            <h3>{todo.title}</h3>
-            <p>{todo.desc}</p>
-            <button className="btn btn-danger" type="button" onClick={()=>onDelete(todo)}>Delete</button>
-        </div>
-    );
-}
